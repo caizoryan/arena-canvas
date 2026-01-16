b9ef9626dbfa15250b625de83cb42e812dba676c
Aaryan Pashine <pashineaaryan@gmail.com>
2026-01-16 18:26:47 -0500

    fix this shit

--- svg.js
+++
@@ -1,4 +0,0 @@
-slice(start?: number, end?: number): ((doc: any) => void)[][]
-Returns a copy of a section of an array.
-For both start and end, a negative index can be used to indicate an offset from the end of the array.
-For example, -2 refers to the second to last element of the array.
